d2266c6c8a792ed027ef35a0e1c2e25aed8b74a3
geovannysan <geovanny1santiago@gmail.com>
2023-07-09 20:27:22 -0500

    nueccvoa agregado

--- js/consultas-micro.js
+++ js/consultas-micro.js
@@ -1,10 +1,16 @@
-
+const toastbody = document.getElementById("toast-body")
+const toaast = document.querySelector('#toast-header')
+const toasheader = document.getElementById("toastheader")
+const spiner = document.getElementById("spiner")
+function Toastainit(he,msg,rm,add) {
+  console.log(he,msg,rm,add)
+  toastbody.innerHTML =""+msg;
+  toasheader.innerHTML=""+he
+  toaast.classList.remove(""+rm)
+  toaast.classList.add(""+add)
+  new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
 const consulta = async (identificado) => {
-  //0908851249 
-  //TTZqNnlucVpuZmJUUXRzcDVSci9QQT09
-  //  0956234413 0956234413
-  //0918381419  
-  //1204795080
   try {
     const { data } = await axios.post("https://rec.netbot.ec/mikroti/PortalApi/GetClientsDetails",
       {
@@ -50,11 +56,12 @@ document.getElementById('btn_enviar_cont').addEventListener('click', function (e
   e.preventDefault()
   let identificado = document.getElementById("numeroid").value
   let lista = document.querySelector("div.estado")
-  if (identificado.length != 10) {
-    jSuites.notification({
-      error: 1, name: 'Ingrese una cédula ',
-      message: 'Formato de 10 dígitos ',
-    })
+  if (identificado.length < 6) {
+    Toastainit("Ingrese una cédula",'Formato de minimo 10 dígitos ',"bg-primary","bg-danger")
+    /*jSuites.notification({
+      error: 1, name: ' ',
+      message: ,
+    })*/
     document.getElementById("metodopago").classList.add("d-none")
     return
   }
@@ -69,21 +76,18 @@ document.getElementById('btn_enviar_cont').addEventListener('click', function (e
       if (ouput.estado == 'exito') {
         this.removeAttribute("disabled");
         //let infomacion = ouput.datos[0];
-        new bootstrap.Toast(document.querySelector('#basicToast')).show();
+
         let { facturacion, servicios, nombre, id, estado } = ouput.datos[0]
         if (servicios == null) {
-          jSuites.notification({
+          Toastainit("No registra servicio","Usuario encontrado:"+nombre,"bg-primary","bg-danger")        
+          /*jSuites.notification({
             error: 2,
             name: 'No registra servicio',
             message: "Usuario encontrado: " + nombre,
-          })
+          })*/
           return
         }
-        jSuites.notification({
-          success: 1,
-          name: 'busqueda exitosa',
-          message: "Usuario encontrado: " + nombre,
-        })
+        Toastainit("Usuario encontrado:",nombre,"bg-danger","bg-primary")
         const { direcciion, perfil, ip, costo, emisor } = servicios[0]
         console.log(estado)
         lista.classList.remove("" + !estado == "ACTIVO" ? "bg-success" : "bg-danger")
@@ -116,11 +120,14 @@ document.getElementById('btn_enviar_cont').addEventListener('click', function (e
       }
       else {
         this.removeAttribute("disabled");
-        jSuites.notification({
-          error: 1,
-          name: 'Usuario no encontrado',
-          message: 'No hubo coincidencia',
-        })
+        Toastainit("hubo un error",'Usuario no encontrado',"bg-primary","bg-danger")
+  
+      
+        /* jSuites.notification({
+           error: 1,
+           name: 'Usuario no encontrado',
+           message: 'No hubo coincidencia',
+         })*/
         this.removeAttribute("disabled");
         // e.path[0].children[0].classList.toggle("d-none")
         document.getElementById("nombre").innerHTML = ".....";
@@ -188,63 +195,43 @@ imageUpload.onchange = function () {
     var fordata = new FormData();
     fordata.append('fileTest', this.files[0]);
     console.log(fordata)
-    
-    //https://povagu.000webhostapp.com/files/20221218152550.jpeg
-    //https://povagu.000webhostapp.com/files/transfer.jpeg
-    //https://povagu.000webhostapp.com/files/pago.jpeg
-    /* OCRAPI("https://povagu.000webhostapp.com/files/20221222133158.jpeg").then(salida => {
-         let respues = salida.responses[0].fullTextAnnotation.text.split("\n")
-         let nuevo = respues.map((e, i) => {
-             if (e.includes("Comprobante")) {
-                  document.getElementById("control").value=""+getNumbersInString(e)
-                 
-                 return valores[0] = getNumbersInString(e)
-             }
-             if (e.includes("$")){
-                 
-                 if ( parseFloat( e.split("$")[1].replace("$",""))>1) document.getElementById("monto").value = "$" + e.split("$")[1]
-                 return valores[1]= getNumbersInString(e.replace("$"," "))    
-             }
-         }).filter(function (value) {
-             return value != undefined;
-         })
-
-         console.log(respues, nuevo)
-         //console.log(salida.responses[0].fullTextAnnotation.text)
-     }
-     ).catch(erro => console.log(erro))*/
+    spiner.classList.remove("d-none")
     axios.post("https://microtikcd.000webhostapp.com/js/guardar.php", fordata).then(respuesta => respuesta)
       .then(decodificado => {
         if (!decodificado.data.status) {
-          jSuites.notification({
+          Toastainit("Hubo un error",""+decodificado.data.result,"bg-primary","bg-danger")
+          spiner.classList.add("d-none")
+         /* jSuites.notification({
             error: 1,
             name: 'Hubo un error',
             message: decodificado.data.result,
-          })
+          })*/
           console.log(decodificado.data);
         }
         if (decodificado.data.status) {
+         
           nota = decodificado.data.result
           OCRAPI(decodificado.data.result).then(salida => {
             console.log(salida)
             if (salida.responses[0].fullTextAnnotation == undefined) {
-              jSuites.notification({
-                error: 1,
-                name: 'Hubo un error',
-                message: "Su comprobante no sera procesado intente con otro o comunuiquese con otro mas",
-              })
+              Toastainit("Hubo un error","Su comprobante no sera procesado intente con otro o comunuiquese con otro mas","bg-primary","bg-danger")
+              spiner.classList.add("d-none")
               console.log(salida)
               return
             }
             let respues = salida.responses[0].fullTextAnnotation.text.toLowerCase().split("\n")
             if (salida.responses[0].fullTextAnnotation.text.toLowerCase().includes("computecnicsnet")) {
+              Toastainit("Hubo un error","Su comprobante no sera procesado intente con otro o comunuiquese con otro mas","bg-primary","bg-danger")
+              spiner.classList.add("d-none")
+              /*
               jSuites.notification({
                 error: 1,
                 name: 'Hubo un error',
                 message: "Su comprobante no sera procesado intente con otro o comunuiquese con otro mas",
-              })
+              })*/
               return
             }
+            spiner.classList.add("d-none")
             console.log(respues, salida.responses[0].fullTextAnnotation.text.toLowerCase().includes("computecnicsnet"))
             if (respues.some(e => e.includes("banco pichincha"))) {
               if (respues.some(e => e.includes("transferencia"))) Pichincha("Transferencia", respues)
@@ -257,13 +244,17 @@ imageUpload.onchange = function () {
             if (respues.some(e => e.includes("banco guayaquil"))) {
               if (respues.some(e => e.includes("transferencia"))) Guayaquil("Transferencia", respues)
               if (respues.some(e => e.includes("deposito"))) Guayaquil("Deposito", respues)
-             
+
             }
           }
-          ).catch(erro => console.log(erro))
+          ).catch(erro =>{
+            spiner.classList.add("d-none")
+            console.log(erro)})
         }
         console.log(decodificado.data);
-      }).catch(error => console.log(error));
+      }).catch(error => {
+        spiner.classList.add("d-none")
+        console.log(error)});
 
   } else {
     console.log(input)
@@ -322,7 +313,7 @@ function Pacifico(tipo, arr) {
   let Total, Comprobante;
   switch (tipo) {
     case "Deposito":
-       Total = arr.map(e => {
+      Total = arr.map(e => {
         if (isNaN(parseFloat(e.match(/\d+(\.\d+)?/g)))) return
         if (!String(e.match(/\d+(\.\d+)?/g)).includes(".")) return
         return String(e.match(/\d+(\.\d+)?/g))
@@ -345,13 +336,13 @@ function Pacifico(tipo, arr) {
       return { Paga: Total, Comprobante: Comprobante }
       break;
     case "Transferencia":
-       Total = arr.map(e => {
+      Total = arr.map(e => {
         if (isNaN(parseFloat(e.match(/\d+(\.\d+)?/g)))) return
         if (!String(e.match(/\d+(\.\d+)?/g)).includes(".")) return
         return String(e.match(/\d+(\.\d+)?/g))
       }).filter(f => f != undefined)
-       Comprobante = arr.find(g => g.includes("No.")).match(/\d+(\.\d+)?/g)
-     
+      Comprobante = arr.find(g => g.includes("No.")).match(/\d+(\.\d+)?/g)
+
       console.log(Math.max(...Total).toFixed(2), String(Comprobante))
       document.getElementById("control").value = "" + Comprobante
       document.getElementById("monto").value = "$" + Math.max(...Total).toFixed(2)
